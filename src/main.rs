d19773511679afd3d36ea4fa1b8154d6046764f3
Trevor Elliott <awesomelyawesome@gmail.com>
2020-03-07 09:31:37 -0800

    Parse markdown

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,14 @@ extern crate clap;
 extern crate comrak;
 extern crate failure;
 
+use comrak::{Arena,ComrakOptions};
 use clap::{App, Arg};
 use failure::{err_msg, Error};
+
+use std::{
+    fs::File,
+    io::{self, Read},
+};
 
 fn main() -> Result<(), Error> {
 
@@ -18,9 +24,36 @@ fn main() -> Result<(), Error> {
              .help("Start a new day"))
         .arg(Arg::with_name("input")
              .index(1)
-             .help("The TODO file to process")
-             .required(true))
+             .help("The TODO file to process"))
         .get_matches();
+
+    let mut buf = String::new();
+    let input = matches.value_of("input");
+    match input {
+        Some("-") | None => {
+            io::stdin().read_to_string(&mut buf)?;
+        },
+        Some(path) => {
+            let mut f = File::open(path)?;
+            f.read_to_string(&mut buf)?;
+        },
+    }
+
+    let opts = {
+        let mut opts = ComrakOptions::default();
+        opts.ext_tasklist = true;
+        opts
+    };
+
+    let arena = Arena::new();
+
+    let doc = comrak::parse_document(&arena, &buf, &opts);
+
+    if matches.is_present("next") {
+        println!("next not implemented");
+    } else {
+        println!("sorting not implemented");
+    }
 
     Ok(())
 }
